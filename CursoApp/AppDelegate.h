c89e144ae583afced2d6a404d6b771e73ac5948e
Elton Mendes Vieira Junior <eltonj@MACCIT011099.local>
2014-06-14 08:44:43 -0300

    Initial Commit

---
+++ CursoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CursoApp
+//
+//  Created by Elton Mendes Vieira Junior on 14/06/14.
+//  Copyright (c) 2014 cit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
